c1f0b72f75254ef84b852e0c918ae856ec51e123
CHARLIE ETIENNE <ce@web-nancy.fr>
2025-09-08 22:09:09 +0200

    simplify implementation

--- packages/forms/src/Components/RichEditor/TipTapExtensions/ImageExtension.php
+++ packages/forms/src/Components/RichEditor/TipTapExtensions/ImageExtension.php
@@ -32,13 +32,7 @@ class ImageExtension extends BaseImage
                 'parseHTML' => fn ($DOMNode) => $DOMNode->getAttribute('data-id') ?: null,
                 'renderHTML' => fn ($attributes) => ['data-id' => $attributes->id ?? null],
             ],
-            'loading' => [
-                'default' => null,
-                'parseHTML' => fn ($DOMNode) => $DOMNode->getAttribute('loading') ?: null,
-                'renderHTML' => fn ($attributes) => [
-                    'loading' => $attributes->loading ?? null,
-                ],
-            ],
+            'loading' => [],
         ];
     }
 }
